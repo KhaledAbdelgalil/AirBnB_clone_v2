6d8e3d6f956173e3f0ad6785bfb7e0afcf33a3e9
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-03 23:49:44 +0100

    task 1 web static deploy4

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ def do_pack():
     today_date = datetime.today()
     date = f"{today_date.year}{today_date.month}{today_date.day}"
     time = f"{today_date.hour}{today_date.minute}{today_date.second}"
-    archive_name = f"versions/we_static_{date}{time}.tgz"
+    archive_name = f"versions/web_static_{date}{time}.tgz"
     local("mkdir -p versions")
     try:
         local(f"tar -cvzf {archive_name} web_static")
