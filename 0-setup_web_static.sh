38a6957f72e9419484298be7888d66a9a35961c2
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-03 22:51:03 +0100

    task 0 web deploy static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+
+echo "<html><head></head><body>Hello World2!</body></html>"> /data/web_static/releases/test/index.html
+
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+sed -i '26i\	location /hbnb_static {\n\t alias /data/web_static/current/;\n} ' /etc/nginx/sites-available/default
+service nginx start
