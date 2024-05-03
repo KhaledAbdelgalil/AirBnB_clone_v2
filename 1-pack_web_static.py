9f9a3cce0bb0d1610596ce84bd4040699eda872e
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-03 23:45:22 +0100

    task 1 web static deploy

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive """
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    today_date = datetime.today()
+    date = f"{today_date.year}{today_date.month:02}{today_date.day:02}"
+    time = f"{today_date.hour:02}{today_date.minute:02}{today_date.second:02}"
+    archive_name = f"versions/we_static_{date}{time}.tgz"
+    local("mkdir -p versions")
+    try:
+        local(f"tar -cvzf {archive_name} web_static")
+        return archive_name
+    except Exception:
+        return None
