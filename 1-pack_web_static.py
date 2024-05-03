9a47f2b0f72d396cecf2d1bacd9e48a5ebab92b7
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-03 23:51:50 +0100

    task 1 web static deploy5

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import local
 
 
 def do_pack():
-    today_date = datetime.today()
+    today_date = datetime.utcnow()
     date = f"{today_date.year}{today_date.month}{today_date.day}"
     time = f"{today_date.hour}{today_date.minute}{today_date.second}"
     archive_name = f"versions/web_static_{date}{time}.tgz"
