6b8a05c5e0b9c0c60c673574fd44526dac69e531
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-04 01:55:17 +0100

    task 2 in web static deploy2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Fabric script that generates a .tgz archive """
-from datetime import datetime
+
+
 from fabric.api import *
 from os import path
 env.hosts = ['54.237.48.43', '35.175.132.199']
