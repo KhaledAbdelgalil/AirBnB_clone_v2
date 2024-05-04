3c8b1912c293e536cf6821e9d6275cfb408059fb
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-04 01:42:32 +0100

    task 2 in web static deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,48 +8,24 @@ env.hosts = ['54.237.48.43', '35.175.132.199']
 
 
 def do_deploy(archive_path):
-    """Create a tar gzipped archive of the directory web_static."""
-    if path.exists(archive_path) is False:
+    """ A function that distributes an archive to your web servers """
+    try:
+        if path.exists(archive_path):
+            file_name = archive_path.split("/")[-1]
+            file_no_ext = file_name.split(".")[0]
+            path = "/data/web_static/releases/"
+            put(archive_path, '/tmp/')
+            run('mkdir -p {}{}/'.format(path, file_no_ext))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                                   path, file_no_ext))
+            run('rm /tmp/{}'.format(file_name))
+            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
+            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {}{}/ /data/web_static/current'.format(
+                path, file_no_ext))
+            return True
+        else:
+            return False
+    except Exception:
         return False
-    put(archive_path, "/tmp/")
-    filename: str = path.basename(archive_path, )
-    filename_without_extension = filename.split(".")[0]
-
-    ''' create required folder and uncompress'''
-    if run(f"mkdir -p /data/web_static/releases/\
-           {filename_without_extension}").failed:
-        return False
-
-    if run(f"rm -rf /data/web_static/releases/\
-           {filename_without_extension}/*").failed:
-        return False
-
-    if run(f"tar -xzf /tmp/{filename} -C /data/web_static/releases/\
-           {filename_without_extension}").failed:
-        return False
-
-    ''' Copy the data from web_static folder to parent folder '''
-    if run(f"mv -uf /data/web_static/releases/\
-           {filename_without_extension}/web_static/* \
-            /data/web_static/releases/\
-            {filename_without_extension}").failed:
-        return False
-
-    if run(f"rm -dr /data/web_static/releases/\
-           {filename_without_extension}/web_static/").failed:
-        return False
-
-    # Delete the archive
-    if run(f"rm /tmp/{filename}").failed:
-        return False
-
-    ''' Delete the symbolic link to '/data/web_static/current' and
-    link it to the newly uncompressed folder'''
-    if run(f'rm -rf /data/web_static/current').failed:
-        return False
-
-    if run(f'ln -s /data/web_static/releases/\
-           {filename_without_extension} /data/web_static/current').failed:
-        return False
-
-    return True
