de3018c535c7c5b31ab9beb3e8d25499cb32872c
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-05 15:35:00 -0700

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,17 +12,18 @@ def do_deploy(archive_path):
     try:
         if exists(archive_path):
             file_name = archive_path.split("/")[-1]
-            file_ext = file_name.split(".")[0]
-            file_no_ext = "web_static"
+            file_no_ext = file_name.split(".")[0]
             path = "/data/web_static/releases/"
             put(archive_path, '/tmp/')
-            run('mkdir -p {}{}/'.format(path, file_ext))
+            run('mkdir -p {}{}/'.format(path, file_no_ext))
             run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
-                                                   path, file_ext))
+                                                   path, file_no_ext))
             run('rm /tmp/{}'.format(file_name))
-            run('mv {0}{1}/{2}/* {0}{1}/'.format(path, file_ext, file_no_ext))
+            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
+            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
             run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(path, file_ext))
+            run('ln -s {}{}/ /data/web_static/current'.format(
+                path, file_no_ext))
             return True
         else:
             return False
