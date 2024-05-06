bc75daa5cfa7fef27bdfdc6aaee9b1b24cf8cb68
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-06 12:45:44 +0100

    final isa of airbnb v2 deploy static6

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 that distributes an archive to your web servers """
 
 
-from fabric.api import local, run, put, env
+from fabric.api import run, put, env
 from os.path import exists
 env.hosts = ['54.237.48.43', '35.175.132.199']
 
@@ -16,17 +16,14 @@ def do_deploy(archive_path):
             file_no_ext = file_name.split(".")[0]
             path = "/data/web_static/releases/"
             put(archive_path, '/tmp/')
-            '''run'''
-            local('mkdir -p {}{}/'.format(path, file_no_ext))
-            '''run'''
-            local('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+            run('mkdir -p {}{}/'.format(path, file_no_ext))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
                                                    path, file_no_ext))
-            '''run'''
-            local('rm /tmp/{}'.format(file_name))
-            local('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
-            local('rm -rf {}{}/web_static'.format(path, file_no_ext))
-            local('rm -rf /data/web_static/current')
-            local('ln -s {}{}/ /data/web_static/current'.format(
+            run('rm /tmp/{}'.format(file_name))
+            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
+            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {}{}/ /data/web_static/current'.format(
                 path, file_no_ext))
             return True
         else:
