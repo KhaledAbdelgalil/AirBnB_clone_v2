f97fc6984c90192162eee36499b1262f90e47d39
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-18 15:54:48 +0100

    task 1 web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,10 +16,12 @@ def default():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     # Run the Flask application
